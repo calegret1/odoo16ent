5c0ef573d2b02b66988bf9ec79b7d6675136f579
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-11-15 18:01:47 +0100

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -2,14 +2,5 @@ echo "Escriba nueva contraseña para el usuario root:
 sudo passwd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 service ssh reload
-while true; do
-    read -p "¿Instalar QEMU Agent? " yn
-    case $yn in
-        [Yy]* ) make install; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
 read -p "Se va a proceder a reiniciar el servidor. Después, inicie sesión por SSH con el usuario root. Presione [Enter] para continuar..."
-reboot
+sudo reboot
