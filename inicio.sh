35b25a5b928928e853019857e2f9e64e9091933f
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-11-15 16:42:18 +0100

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -2,3 +2,5 @@ echo "Escriba nueva contraseña para el usuario root:
 sudo passwd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 service ssh reload
+read -p "Se va a proceder a reiniciar el servidor. Después, inicie sesión por SSH con el usuario root. Presione [Enter] para continuar..."
+reboot
