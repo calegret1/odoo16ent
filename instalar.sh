b3fa8a621bf54bd2033dba8c55dd3bd3d324bc2f
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-11-16 12:58:09 +0100

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -8,6 +8,11 @@ sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /e
 apt-get update
 apt-get upgrade -y
 apt install postgresql -y
+apt install python3-pip -y
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+apt --fix-broken install -y
+dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 git clone https://github.com/calegret1/odoo16ent.git
 cd ./odoo16ent/install/
 cat odoo16ent.* > odoo16ent.deb
@@ -15,4 +20,8 @@ rm odoo16ent.aa odoo16ent.ab odoo16ent.ac
 dpkg -i odoo16ent.deb
 apt-get install -f -y
 dpkg -i odoo16ent.deb
+apt-get update
+apt-get upgrade -y
+service odoo restart
+
 sed -i 's/$nrconf{restart} = '"'"'a'"'"';/#$nrconf{restart} = '"'"'i'"'"';/g' /etc/needrestart/needrestart.conf
