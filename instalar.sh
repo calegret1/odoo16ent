25c82872f542eaa890e8907c6d6854ecf433df75
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-11-16 12:18:45 +0100

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -4,13 +4,15 @@
 #cd odoo16ent
 #./instalar.sh
 
+sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 apt-get update
 apt-get upgrade -y
-sudo apt install postgresql -y
+apt install postgresql -y
 git clone https://github.com/calegret1/odoo16ent.git
 cd ./odoo16ent/install/
 cat odoo16ent.* > odoo16ent.deb
 rm odoo16ent.aa odoo16ent.ab odoo16ent.ac
-dpkg -i odoo16ent.deb -y
-apt-get install -f -y
-dpkg -i odoo16ent.deb -y
+dpkg -i odoo16ent.deb
+dpkg -i odoo16ent.deb apt-get install -f -y
+dpkg -i odoo16ent.deb dpkg -i odoo16ent.deb
+sed -i 's/$nrconf{restart} = '"'"'a'"'"';/#$nrconf{restart} = '"'"'i'"'"';/g' /etc/needrestart/needrestart.conf
