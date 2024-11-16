86707fdac464d29a192004aa06b674223e6a7e4e
calegret1 <155742510+calegret1@users.noreply.github.com>
2024-11-16 11:47:13 +0100

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,3 +1,8 @@
 apt-get update
 apt-get upgrade
 sudo apt install postgresql -y
+git clone https://github.com/calegret1/odoo16ent.git
+cd ./odoo16ent/install/
+cat odoo16ent.* > odoo16ent.deb
+rm odoo16ent.aa odoo16ent.ab odoo16ent.ac
+dpkg -i odoo16ent.deb
